4a0aeada745f9619f51ba9be7c327973781e44a0
Alberto <beto181295@gmail.com>
2015-01-02 21:22:22 +0100

    Cambios en C y estructura de la iteración ASM

--- dftsse.asm
+++ dftsse.asm
@@ -1,45 +1,60 @@
-
-
 segment .data
 
 segment .bss
 
 segmet .text
-	global _start
-	extern x
+	global dftsse
+	extern val_complex
+	extern res_complex
 
-	_start:
+	dftsse:
+		mov ecx, 1024
+		LoopJ:
+		mov ebx, ecx 
 
-	movddup xmm0, qword[x]
-	movddup xmm2, qword[x+8]
-	call angulo
-	fsin
-	movlpd xmm1, st0
-	movhpd xmm3, st0
-	call angulo
-	fcos
-	movhpd xmm1, st0
-	movlpd xmm3, st0
-	mulpd xmm0, xmm1
-	mulpd xmm2, xmm3
-	addsubpd xmm0, xmm2
+			mov ecx, 1024
+			mov eax, 0
+			LoopK:
+				movddup xmm0, [val_complex+eax]
+				movddup xmm2, [val_complex+eax+8]
+				call Angulo
+				fsin
+				movlpd xmm1, st0
+				movhpd xmm3, st0
+				call Angulo
+				fcos
+				movhpd xmm1, st0
+				movlpd xmm3, st0
+				mulpd xmm0, xmm1
+				mulpd xmm2, xmm3
+				addsubpd xmm0, xmm2
+				
+				movapd xmm1, [res_complex+eax]
+				addpd xmm0, xmm1
+				movapd [res_complex+eax], xmm0
+
+				add eax, 16
+				loop LoopK
+
+		mov ecx, ebx
+		loop LoopJ
 
 
 
 
 
 
-	angulo:
-		finit
-		fild 1024
-		fldpi
-		fldpi
-		faddp
-		fild -1
-		fmulp
-		fdivp
-		fld qword[j]
-		fmulp
-		fld qword[k]
-		fmulp
-		ret
+		Angulo:
+			finit
+			fild 1024
+			fldpi
+			fldpi
+			faddp
+			fild -1
+			fmulp
+			fdivp
+			fld qword[j]
+			fmulp
+			fld qword[k]
+			fmulp
+			ret
--- pract3.c
+++ pract3.c
@@ -5,43 +5,45 @@
 #define PI 3.14
 #define ANGULO(j, k) (((-2*PI)/TAM)*(j)*(k)) 
 
-double f_real(int j);
-double f_imagin(int j);
+void dftc();
+// void dftsse();
+void imprime_resul();
 
-double x[TAM][2];
+double val_complex[TAM][2];
+double res_complex[TAM][2];
 
 int main() {
 	int i;
 	srand48(time(NULL));
 	for (i = 0; i < TAM; i++) {
-		x[i][0] = (drand48() * 100) - 50;
-		x[i][1] = (drand48() * 100) - 50;
+		val_complex[i][0] = (drand48() * 100) - 50;
+		val_complex[i][1] = (drand48() * 100) - 50;
 	}
 
-	for (i = 0; i < TAM; i++) {
-		printf("Indice: %d \t|\t Numero: (%lf)+(%lf)i \t|\t Transformada: (%lf)+(%lf)i\n", 
-				i, x[i][0], x[i][1], f_real(i), f_imagin(i));
-	}
+	dftc();
+	imprime_resul();
+	// dftsse();
+	// imprime_resul();
 
 	return 0;
 }
 
-double f_real(int j) {
-	int i;
-	double freal = 0;
-	for (i = 0; i <= j; i++) {
-		freal += x[i][0] * cos(ANGULO(j, i)) - x[i][1] * sin(ANGULO(j, i));
-	}
+void dftc() {
+	int i, j;
 
-	return freal;
+	for (i = 0; i < TAM; i++) {
+		for (j = 0; j < TAM; j++) {
+			res_complex[i][0] += val_complex[j][0] * cos(ANGULO(i, j)) - val_complex[j][1] * sin(ANGULO(i, j));
+			res_complex[i][1] += val_complex[j][0] * sin(ANGULO(i, j)) + val_complex[j][1] * cos(ANGULO(i, j));
+		}
+	}
 }
 
-double f_imagin(int j) {
+void imprime_resul() {
 	int i;
-	double fimagin = 0;
-	for (i = 0; i <= j; i++) {
-		fimagin += x[i][0] * sin(ANGULO(j, i)) + x[i][1] * cos(ANGULO(j, i));
-	}
 
-	return fimagin;
+	for (i = 0; i < TAM; i++) {
+		printf("Indice: %d \t|\t Numero: %lf%+lfi \t|\t Transformada: %lf%+lfi\n", 
+				i, val_complex[i][0], val_complex[i][1], res_complex[i][0], res_complex[i][1]);
+	}
 }
