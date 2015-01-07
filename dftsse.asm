fee9bea083771289aa5f7510a701af06ef56f806
Alberto <beto181295@gmail.com>
2015-01-07 21:28:19 +0100

    Retoques hechos en clase

--- dftsse.asm
+++ dftsse.asm
@@ -1,56 +1,65 @@
 segment .data
+	j dq 0
+	k dq 0
+	menosuno dq -1
+	angulo dq 0
+	zero dq 0
+	TAM dq 1024
 
-segment .bss
-
-segmet .text
+segment .text
 	global dftsse
+
 	extern val_complex
 	extern res_complex
 
 	dftsse:
 		mov ecx, 1024
+		mov edx, 0								; edx: indice de res_complex
 		LoopJ:
-		mov ebx, ecx 
-
-			mov ecx, 1024
-			mov eax, 0
+			dec ecx
+			mov ebx, ecx 							; Salvamos el contador del bucle J									
+			mov ecx, 1024							; Inicializamos los registros usados para el sumatorio
+			mov eax, 0								; ecx: 1024 repeticiones, eax: indice numeros complejos
+			movupd xmm4, [zero]							; xmm4: acumulador para la suma
 			LoopK:
-				movddup xmm0, [val_complex+eax]
-				movddup xmm2, [val_complex+eax+8]
+				movddup xmm0, [val_complex+eax]		; xmm0 = parte real del numero x(subk) duplicada
+				movddup xmm2, [val_complex+eax+8]	; xmm2 = parte imaginaria del numero x(subk) duplicada
 				call Angulo
 				fsin
-				movlpd xmm1, st0
-				movhpd xmm3, st0
+				fstp qword[angulo]
+				movlpd xmm1, [angulo]					; xmm1(baja) = sin(-2*pi*j*k/1024)
+				movhpd xmm3, [angulo]					; xmm3(alta) =  xmm1(baja)
 				call Angulo
 				fcos
-				movhpd xmm1, st0
-				movlpd xmm3, st0
-				mulpd xmm0, xmm1
-				mulpd xmm2, xmm3
-				addsubpd xmm0, xmm2
-				
-				movapd xmm1, [res_complex+eax]
-				addpd xmm0, xmm1
-				movapd [res_complex+eax], xmm0
+				fstp qword[angulo]
+				movhpd xmm1, [angulo]					; xmm1(alta) = cos(-2*pi*j*k/1024)
+				movlpd xmm3, [angulo]					; xmm3(baja) = xmm1(alta)
+				mulpd xmm0, xmm1					; xmm0(baja, alta) = parte real * (baja)sin y (alta)cos
+				mulpd xmm2, xmm3					; xmm2(baja, alta) =  parte imaginaria * (baja)cos y (alta)sin
+				addsubpd xmm0, xmm2					; xmm0 = (baja)parte imaginaria del producto, (alta)parte real
+				addpd xmm4, xmm0					; Acumulamos en xmm4 el resultado 
+				add eax, 16							; Actualizamos el indice para conseguir el siguiente complejo
+				inc qword[k]
+				dec ecx
+				cmp ecx, 0
+				jne LoopK
 
-				add eax, 16
-				loop LoopK
+			movupd [res_complex+edx], xmm4			; Almacenamos el resultado de cada dft
+			add edx, 16
 
-		mov ecx, ebx
-		loop LoopJ
-
-
-
-
-
+			mov ecx, ebx
+			inc qword[j]
+		cmp ecx, 0
+		jne LoopJ
+		ret
 
 		Angulo:
 			finit
-			fild 1024
+			fild qword[TAM]
 			fldpi
 			fldpi
 			faddp
-			fild -1
+			fild qword[menosuno]
 			fmulp
 			fdivp
 			fld qword[j]
--- pract3.c
+++ pract3.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 #define TAM 1024
 #define PI 3.14
 #define ANGULO(j, k) (((-2*PI)/TAM)*(j)*(k)) 
 
 void dftc();
-// void dftsse();
+double dftsse();
 void imprime_resul();
 
 double val_complex[TAM][2];
@@ -19,23 +20,41 @@ int main() {
 		val_complex[i][0] = (drand48() * 100) - 50;
 		val_complex[i][1] = (drand48() * 100) - 50;
 	}
-
+	time_t inicioa, finala, iniciob, finalb;
+	
+	inicioa = time(NULL);
 	dftc();
+	finala = time(NULL);
+	//double enc = difftime (final, inicio);
 	imprime_resul();
-	// dftsse();
-	// imprime_resul();
+	iniciob = time(NULL);
+	dftsse();
+	finalb = time(NULL);
+printf("%llu\n", (unsigned long long)inicioa);
+printf("%llu\n", (unsigned long long)finala);
+printf("%llu\n", (unsigned long long)iniciob);
+printf("%llu\n", (unsigned long long)finalb);
+	//printf("%f, %f\n", difftime (final, inicio), enc);
+	//imprime_resul();
 
 	return 0;
 }
 
 void dftc() {
 	int i, j;
+	double res_real, res_imagin;
 
 	for (i = 0; i < TAM; i++) {
+		res_real = 0;
+		res_imagin = 0;
+
 		for (j = 0; j < TAM; j++) {
-			res_complex[i][0] += val_complex[j][0] * cos(ANGULO(i, j)) - val_complex[j][1] * sin(ANGULO(i, j));
-			res_complex[i][1] += val_complex[j][0] * sin(ANGULO(i, j)) + val_complex[j][1] * cos(ANGULO(i, j));
+			res_real += val_complex[j][0] * cos(ANGULO(i, j)) - val_complex[j][1] * sin(ANGULO(i, j));
+			res_imagin += val_complex[j][0] * sin(ANGULO(i, j)) + val_complex[j][1] * cos(ANGULO(i, j));
 		}
+
+		res_complex[i][0] = res_real;
+		res_complex[i][1] = res_imagin;
 	}
 }
 
