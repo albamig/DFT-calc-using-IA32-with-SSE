8fa6dd63ed412197c09d0e9220c64a252fc36286
Alberto <beto181295@gmail.com>
2015-01-16 13:47:48 +0100

    Código preparado para entregar

--- dftsse.asm
+++ dftsse.asm
@@ -1,3 +1,9 @@
+; Desarrollado por Sergio Delgado y Alberto Amigo
+;
+; En este código se calculan las DFT de un conjunto de números implementando 
+; un eficiente algoritmo de multiplicacion de números complejos
+; utilizando la extensión SSE de la arquitectura Intel IA32
+
 segment .data
 	j dq 0
 	k dq 0
@@ -9,31 +15,31 @@ segment .data
 segment .text
 	global dftsse
 
-	extern val_complex
-	extern res_complex
+	extern val_complex								; matriz de valores
+	extern res_complex								; matriz de resultados
 
 	dftsse:
-		mov ecx, 1024
-		mov edx, 0								; edx: indice de res_complex
+		mov ecx, dword[TAM]
+		mov edx, 0									; edx: indice de res_complex
 		LoopJ:
 			dec ecx
 			mov ebx, ecx 							; Salvamos el contador del bucle J									
-			mov ecx, 1024							; Inicializamos los registros usados para el sumatorio
+			mov ecx, dword[TAM]						; Inicializamos los registros usados para el sumatorio
 			mov eax, 0								; ecx: 1024 repeticiones, eax: indice numeros complejos
-			movupd xmm4, [zero]							; xmm4: acumulador para la suma
+			movupd xmm4, [zero]						; xmm4: acumulador para la suma
 			LoopK:
 				movddup xmm0, [val_complex+eax]		; xmm0 = parte real del numero x(subk) duplicada
 				movddup xmm2, [val_complex+eax+8]	; xmm2 = parte imaginaria del numero x(subk) duplicada
 				call Angulo
 				fsin
 				fstp qword[angulo]
-				movhpd xmm1, [angulo]					; xmm1(alta) = sin(-2*pi*j*k/1024)
-				movlpd xmm3, [angulo]					; xmm3(baja) =  xmm1(baja)
+				movhpd xmm1, [angulo]				; xmm1(alta) = sin(-2*pi*j*k/1024)
+				movlpd xmm3, [angulo]				; xmm3(baja) =  xmm1(baja)
 				call Angulo
 				fcos
 				fstp qword[angulo]
-				movlpd xmm1, [angulo]					; xmm1(baja) = cos(-2*pi*j*k/1024)
-				movhpd xmm3, [angulo]					; xmm3(alta) = xmm1(alta)
+				movlpd xmm1, [angulo]				; xmm1(baja) = cos(-2*pi*j*k/1024)
+				movhpd xmm3, [angulo]				; xmm3(alta) = xmm1(alta)
 				mulpd xmm0, xmm1					; xmm0(baja, alta) = parte real * (baja)sin y (alta)cos
 				mulpd xmm2, xmm3					; xmm2(baja, alta) =  parte imaginaria * (baja)cos y (alta)sin
 				addsubpd xmm0, xmm2					; xmm0 = (baja)parte imaginaria del producto, (alta)parte real
@@ -53,20 +59,18 @@ segment .text
 		jne LoopJ
 		ret
 
-		Angulo:
-			finit
-			
-b1:
+		;
+		; Calcula -2*pi*j*k/TAM
+		;
+		Angulo:		
+			finit	
 			fldpi
 			fldpi
 			faddp
-b2:
 			fild qword[menosuno]
 			fmulp
 			fild qword[TAM]
-b3:
 			fdivp
-b4:
 			fild qword[j]
 			fmulp
 			fild qword[k]
--- pract3.c
+++ pract3.c
@@ -1,3 +1,5 @@
+// Desarrollado por Sergio Delgado y Alberto Amigo
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -6,32 +8,30 @@
 #define ANGULO(j, k) (((-2*M_PI)/TAM)*(j)*(k)) 
 
 void dftc();
-double dftsse();
+void dftsse();
 void imprime_resul();
+void genera_num();
 
 double val_complex[TAM][2];
 double res_complex[TAM][2];
 
 int main() {
-	int i;
-	srand48(time(NULL));
-	for (i = 0; i < TAM; i++) {
-	 	val_complex[i][0] = (drand48() * 100) - 50;
-		val_complex[i][1] = (drand48() * 100) - 50;
-	}
+	genera_num();
 	clock_t ini_c, fin_c, ini_sse, fin_sse;
 
+	// Calculo de la DFT en ANSI C
 	ini_c = clock();
 	dftc();
 	fin_c = clock();
 
 	imprime_resul();
 
+	// Calculo de la  DFT en IA32
 	ini_sse = clock();
 	dftsse();
 	fin_sse = clock();
 
-	imprime_resul();
+	// imprime_resul(); // Descomentar esta linea para comprobar que el cálculo es correcto
 
 	printf ("Resutado C: %f\nResultado SSE: %f\n", ((double)fin_c-ini_c)/CLOCKS_PER_SEC, ((double)fin_sse-ini_sse)/CLOCKS_PER_SEC);
 
@@ -40,19 +40,12 @@ int main() {
 
 void dftc() {
 	int i, j;
-	//double res_real, res_imagin;
 
 	for (i = 0; i < TAM; i++) {
-		//res_real = 0;
-		//res_imagin = 0;
-
 		for (j = 0; j < TAM; j++) {
 			res_complex[i][0] += val_complex[j][0] * cos(ANGULO(i, j)) - val_complex[j][1] * sin(ANGULO(i, j));
 			res_complex[i][1] += val_complex[j][0] * sin(ANGULO(i, j)) + val_complex[j][1] * cos(ANGULO(i, j));
 		}
-
-		//res_complex[i][0] = res_real;
-		 //= res_imagin;
 	}
 }
 
@@ -62,5 +55,15 @@ void imprime_resul() {
 	for (i = 0; i < TAM; i++) {
 		printf("Indice: %d \t|\t Numero: %lf%+lfi \t|\t Transformada: %lf%+lfi\n", 
 				i, val_complex[i][0], val_complex[i][1], res_complex[i][0], res_complex[i][1]);
+	}
+}
+
+void genera_num() {
+	int i;
+
+	srand48(time(NULL));
+	for (i = 0; i < TAM; i++) {
+	 	val_complex[i][0] = (drand48() * 100) - 50;
+		val_complex[i][1] = (drand48() * 100) - 50;
 	}
 }
